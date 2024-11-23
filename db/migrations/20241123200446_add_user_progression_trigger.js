a8617b62264b52fdccca6608f8cd48348984ad76
betelhemalemayehu <Betelhem.L.A@gmail.com>
2024-11-23 16:20:32 -0500

    added trigger function so each new user creates rows in progression table for each pose with a default status of not attempted

---
+++ db/migrations/20241123200446_add_user_progression_trigger.js
@@ -0,0 +1,31 @@
+//add_user_progression_trigger.js
+export async function up(knex) {
+  await knex.raw(`
+    CREATE TRIGGER after_user_insert
+    AFTER INSERT ON users
+    FOR EACH ROW
+    BEGIN
+      -- Insert a progression for each pose
+      INSERT INTO progressions (user_id, pose_id, status, created_at, updated_at)
+      SELECT NEW.id, id, 'In Progress', NOW(), NOW()
+      FROM poses;
+
+      -- Insert corresponding media entries using pose's url_png
+      INSERT INTO media (progression_id, custom_media, created_at, updated_at)
+      SELECT
+        p.id,
+        pos.url_png,
+        NOW(),
+        NOW()
+      FROM progressions p
+      JOIN poses pos ON p.pose_id = pos.id
+      WHERE p.user_id = NEW.id;
+    END;
+  `);
+}
+
+export async function down(knex) {
+  await knex.raw(`
+    DROP TRIGGER IF EXISTS after_user_insert;
+  `);
+}
