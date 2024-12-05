51191df3eff13790371db3c9d60be79447a99e1c
betelhemalemayehu <Betelhem.L.A@gmail.com>
2024-12-05 11:49:05 -0500

    removed development from knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,19 +4,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  development: {
-    client: "mysql2",
-    connection: {
-      host: process.env.DATABASE_HOST,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-    },
-    migrations: {
-      directory: "./db/migrations",
-    },
-    seeds: {
-      directory: "./db/seeds",
-    },
+  // development: {
+  client: "mysql2",
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
   },
+  migrations: {
+    directory: "./db/migrations",
+  },
+  seeds: {
+    directory: "./db/seeds",
+  },
+  // },
 };
